9d462249df9c9f755a93e833b6854ac845fd3b2b
Seth Carbon <github.com@genkisugi.net>
2017-09-12 04:52:29 -0700

    acceptable minimal use of new class for query

--- tests/core.test.js
+++ tests/core.test.js
@@ -24,6 +24,9 @@ var jquery_engine = require('bbop-rest-manager').jquery;
 var node_engine = require('bbop-rest-manager').node;
 
 var wikidata = 'https://query.wikidata.org/sparql';
+// Get PMID:9999.
+var wikiquery_all = 'PREFIX wd: <http://www.wikidata.org/entity/> PREFIX wdt: <http://www.wikidata.org/prop/direct/> SELECT ?rtcl ?title ?author ?journal ?date WHERE { ?rtcl wdt:P698 "999". OPTIONAL { ?rtcl wdt:P1476 ?title. } OPTIONAL { ?rtcl wdt:P2093 ?author. } OPTIONAL { ?rtcl wdt:P1433 ?journal. } OPTIONAL { ?rtcl wdt:P577 ?date. } } LIMIT 1';
+var wikiquery_sans = 'SELECT ?rtcl ?title ?author ?journal ?date WHERE { ?rtcl wdt:P698 "999". OPTIONAL { ?rtcl wdt:P1476 ?title. } OPTIONAL { ?rtcl wdt:P2093 ?author. } OPTIONAL { ?rtcl wdt:P1433 ?journal. } OPTIONAL { ?rtcl wdt:P577 ?date. } } LIMIT 1';
 
 ///
 /// Start unit testing.
@@ -61,4 +64,45 @@ describe('bbop-manager-sparql inner ops', function(){
 	m.add_prefix('a', 'b');
     	assert.equal(m.prefixes()[1][1], 'b', 'simple: add prefix');
     });
+});
+
+
+describe('bbop-manager-sparql simple', function(){
+
+    it('trying wikidata', function(done){
+
+	var engine_to_use = new node_engine(response_json);
+	engine_to_use.headers([['accept', 'application/sparql-results+json']]);
+	
+    	// No action, so it doesn't matter what we use.
+    	var m = new manager(wikidata,
+			    [],
+			    response_json,
+			    engine_to_use,
+			    'async');
+
+	m.query(wikiquery_all).then(function(resp){
+	    //console.log('resp',resp);
+    	    assert.isDefined(resp.raw()['head'], 'has json head');
+    	    assert.isDefined(resp.raw()['results'], 'has json results');
+    	    done();
+	}).done();
+    });
+
+    // it('trying the basic prefix ops', function(){
+
+    // 	var engine_to_use = new node_engine(response_json);
+
+    // 	// No action, so it doesn't matter what we use.
+    // 	var m = new manager(wikidata, [['fb', 'foo:bar']], response_base,
+    // 			   engine_to_use);
+
+    // 	// Init.
+    // 	assert.equal(m.prefixes().length, 1, 'simple: prefixes are (a)');
+    // 	assert.equal(m.prefixes()[0][1], 'foo:bar', 'simple: prefixes are (b)');
+
+    // 	// Add.
+    // 	m.add_prefix('a', 'b');
+    // 	assert.equal(m.prefixes()[1][1], 'b', 'simple: add prefix');
+    // });
 });
